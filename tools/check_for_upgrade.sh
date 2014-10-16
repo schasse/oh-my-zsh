ca9d71f9c5d0a32ea1170a0215d03e3b02d8792b
schasse <sebastian.schasse@gapfish.com>
2014-10-16 23:42:21 +0200

    update more than just oh-my-zsh

--- tools/check_for_upgrade.sh
+++ tools/check_for_upgrade.sh
@@ -9,7 +9,7 @@ function _update_zsh_update() {
 }
 
 function _upgrade_zsh() {
-  env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh
+  /bin/sh $HOME/.dotfiles/script/upgrade.sh
   # update the zsh file
   _update_zsh_update
 }
@@ -41,8 +41,8 @@ then
     then
       _upgrade_zsh
     else
-      echo "[Oh My Zsh] Would you like to check for updates?"
-      echo "Type Y to update oh-my-zsh: \c"
+      echo "Would you like to check for updates?"
+      echo "Type Y to update: \c"
       read line
       if [ "$line" = Y ] || [ "$line" = y ]; then
         _upgrade_zsh
@@ -55,4 +55,3 @@ else
   # create the zsh file
   _update_zsh_update
 fi
-
